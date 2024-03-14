003a2b2ba1155ad6a93835a59af6a387b366cfd0
Maulana <maulanadwicahyana@gmail.com>
2024-03-14 20:11:19 +0700

    JS4 DONE

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,17 +7,24 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID level pengguna</th>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>TD Level Pengguna</td>
+                <td>Kode Level</td>
+                <td>Nama Level</td>
+                <td>Aksi</td>
             </tr>
             @foreach ($data as $d)
+            
                 <tr>
                     <td>{{ $d->user_id }}</td>
                     <td>{{ $d->username }}</td>
                     <td>{{ $d->nama }}</td>
                     <td>{{ $d->level_id }}</td>
+                    <td>{{ $d->level->level_kode }}</td>
+                    <td>{{ $d->level->level_nama }}</td>
+                    <td><a href="/user/ubah/{{$d->user_id}}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
                 </tr>
             @endforeach
         </table>
