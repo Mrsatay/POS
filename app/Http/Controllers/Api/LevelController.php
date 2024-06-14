09f8d85c759a2194c285900df2fcf5377caa7d9b
Maulana <maulanadwicahyana@gmail.com>
2024-06-14 16:27:05 +0700

    js 10 selesai

--- app/Http/Controllers/Api/LevelController.php
+++ app/Http/Controllers/Api/LevelController.php
@@ -1,42 +1,42 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\LevelModel;
+use Illuminate\Http\Request;
 
 class LevelController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         return LevelModel::all();
     }
 
-    public function store(Request $request)
-    {
-        $level = LevelModel::create($request->all());
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'level_kode' => 'required|unique:m_level|max:10',
+            'level_nama' => 'required|max:100',
+        ]);
+    
+        $level = LevelModel::create($validatedData);
         return response()->json($level, 201);
     }
+    
 
-    public function show(LevelModel $level)
-    {
+    public function show(LevelModel $level) {
         return LevelModel::find($level);
     }
 
-    public function update(Request $request, LevelModel $level)
-    {
+    public function update(Request $request, LevelModel $level) {
         $level->update($request->all());
         return LevelModel::find($level);
     }
 
-    public function destroy(LevelModel $user)
-    {
-        $user->delete();
-
+    public function destroy(LevelModel $level) {
+        $level->delete();
         return response()->json([
             'success' => true,
-            'message' => 'Data terhapus',
+            'message' => 'Data berhasil dihapus!'
         ]);
     }
 }
